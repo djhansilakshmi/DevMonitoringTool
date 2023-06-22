b123369110e3ee9af1752165f87b298f56e28b5d
Gopi P <pgopirmd@gmail.com>
2023-06-22 22:42:32 +0530

    Dashboard changes moved to client project

--- DeveloperDashboardClient/Program.cs
+++ DeveloperDashboardClient/Program.cs
@@ -1,5 +1,6 @@
 using DeveloperDashboard.DataServices;
 using DeveloperDashboardClient;
+using DeveloperDashboardClient.Data;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<WeatherForecastService>();
 
 builder.Services.AddHttpClient<IAlbumDataService, AlbumDataService>
     (spds => spds.BaseAddress = new Uri(builder.Configuration["api_jsonplaceholder_base_url"]));
